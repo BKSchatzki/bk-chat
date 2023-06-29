9906dc2ff2ddb07683c14261317c56e130da9962
BKSchatzki <106890912+BKSchatzki@users.noreply.github.com>
2023-06-29 09:03:27 -0700

    Add database query and render in DOM

--- src/appwriteConfig.js
+++ src/appwriteConfig.js
@@ -1,9 +1,15 @@
-import { Client } from 'appwrite';
+import { Client, Databases } from 'appwrite';
+
+export const PROJECT_ID = '649c9624c091fba90803';
+export const DATABASE_ID = '649da4e06287410a5a75';
+export const COLLECTION_ID_MESSAGES = '649da4e56a3e5e40252e';
 
 const client = new Client();
 
 client
     .setEndpoint('https://cloud.appwrite.io/v1')
     .setProject('649c9624c091fba90803');
+
+export const databases = new Databases(client);
 
 export default client;
--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { databases, DATABASE_ID, COLLECTION_ID_MESSAGES } from '../appwriteConfig'
 
 const Room = () => {
+
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    getMessages()
+  }, [])
+
+  const getMessages = async () => {
+    const response = await databases.listDocuments(DATABASE_ID, COLLECTION_ID_MESSAGES)
+    console.log('RESPONSE:', response);
+    setMessages(response.documents);
+  }
+
   return (
     <div>
-      ROOM
+      <div>
+        {messages.map(message => (
+          <div key={message.$id}>
+            
+            <div>
+              <p>{message.$createdAt}</p>
+            </div>
+            
+            <div>
+              <span>{message.body}</span>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
