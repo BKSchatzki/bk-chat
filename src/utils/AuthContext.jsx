a759f85ad66f855a6d67c70cf9c9ccf5b8fba72d
BKSchatzki <106890912+BKSchatzki@users.noreply.github.com>
2023-06-30 14:56:54 -0700

    Add getUserOnLoad for persistent session

--- src/utils/AuthContext.jsx
+++ src/utils/AuthContext.jsx
@@ -12,8 +12,18 @@ export const AuthProvider = ({children}) => {
   const [user, setUser] = useState(null)
 
   useEffect(() => {
-    setLoading(false)
+    getUserOnLoad();
   }, [])
+
+  const getUserOnLoad = () => {
+    try {
+      const accountDetails = account.get();
+      setUser(accountDetails);
+    } catch(error) {
+      console.error(error);
+    }
+    setLoading(false);
+  }
 
   const handleUserLogin = async (e, credentials) => {
     e.preventDefault();
