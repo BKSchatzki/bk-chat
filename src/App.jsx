1607eb1e8928ff65e1c73a048c94dc59c88e24cd
BKSchatzki <106890912+BKSchatzki@users.noreply.github.com>
2023-06-30 13:05:28 -0700

    Add react-router-dom, routes, LoginPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Room from './pages/Room'
+import LoginPage from './pages/LoginPage'
 
 function App() {
 
   return (
-    <>
+    <Router>
+      <Routes>
+        <Route path='/login' element={<LoginPage/>} />
+        <Route path='/' element={<Root/>} />
+      </Routes>
       <Room/>
-    </>
+    </Router>
   )
 }
 
