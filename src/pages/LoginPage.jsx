7085ba44b0ec067fb7c62219b0fecc9d82053051
BKSchatzki <106890912+BKSchatzki@users.noreply.github.com>
2023-06-30 18:13:56 -0700

    Create RegisterPage

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../utils/AuthContext';
 
 const LoginPage = () => {
@@ -60,6 +60,8 @@ const LoginPage = () => {
             />
           </div>
         </form>
+
+        <p>Don't have an account? Register <Link to="/register">here</Link>.</p>
       </div>
     </div>
   )
