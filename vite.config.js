b19a75c552d7ecc7a5e6012a247a8a17c86ce06f
BKSchatzki <106890912+BKSchatzki@users.noreply.github.com>
2023-07-02 23:46:06 -0700

    Comment out CSP from vite config and index

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,13 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    headers: {
-      // 'Content-Security-Policy': `style-src 'nonce-random' 'self'`,
-      // Above is from TP. Below is new.
-      "Content-Security-Policy": "script-src 'self' 'wasm-unsafe-eval' 'inline-speculation-rules' https://bkschatzki.github.io",
-    },
-  },
-  base: '/chat-appwrite',
+  // server: {
+  //   headers: {
+  //     // 'Content-Security-Policy': `style-src 'nonce-random' 'self'`,
+  //     // Above is from TP. Below is new.
+  //     "Content-Security-Policy": "script-src 'self' 'wasm-unsafe-eval' 'inline-speculation-rules' https://bkschatzki.github.io",
+  //   },
+  // },
+  // base: '/chat-appwrite',
   plugins: [react()],
 })
