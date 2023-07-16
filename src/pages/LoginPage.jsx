4b405004d8988aafbe25effb8e03f8f820aa5f07
BKSchatzki <106890912+BKSchatzki@users.noreply.github.com>
2023-07-15 18:57:58 -0700

    Change Login gaps to margin-bottom

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -32,10 +32,10 @@ const LoginPage = () => {
             handleUserLogin(e, credentials);
           }}
         >
-          <div className="field--wrapper flex flex-col gap-4">
+          <div className="field--wrapper flex flex-col mb-4">
             {/* <label htmlFor="login__email">Email:</label> */}
             <input
-              className="bg-slate-700 border-0 border-b-2 border-slate-500 rounded-lg w-full outline-0 p-4 shadow-slate-500/50 shadow-md"
+              className="bg-slate-700 border-0 border-b-2 border-slate-500 rounded-lg w-full outline-0 p-4 shadow-slate-500/50 shadow-md transition ease-in-out focus:-translate-y-1 focus:shadow-lg"
               id="login__email"
               name="email"
               placeholder="Enter your email..."
@@ -45,7 +45,7 @@ const LoginPage = () => {
               onChange={handleInputChange}
             />
           </div>
-          <div className="field--wrapper flex flex-col gap-4">
+          <div className="field--wrapper flex flex-col mb-4">
             {/* <label htmlFor="password">Password:</label> */}
             <input
               className="bg-slate-700 border-0 border-b-2 border-slate-500 rounded-lg w-full outline-0 p-4 shadow-slate-500/50 shadow-md"
@@ -58,7 +58,7 @@ const LoginPage = () => {
               onChange={handleInputChange}
             />
           </div>
-          <div className="field--wrapper flex flex-col">
+          <div className="field--wrapper flex flex-col mb-4">
             <input
               className="btn btn--lg btn--main p-4 rounded-lg cursor-pointer bg-gradient-to-b from-green-500 to-emerald-500 shadow-emerald-300/50 shadow-md font-bold uppercase transition ease-in-out active:translate-y-1 active:from-green-600 active:to-emerald-600"
               type="submit"
@@ -66,7 +66,7 @@ const LoginPage = () => {
             />
           </div>
         </form>
-        <p className="text-center mt-4">
+        <p className="text-center">
           Don't have an account?{" "}
           <Link
             className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-500 to-emerald-500 hover:from-green-300 hover:to-emerald-300"
